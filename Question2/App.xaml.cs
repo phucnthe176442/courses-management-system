8eca5d1fd953314a3801432275519ecdfc6abdb3
phucnthe176442 <phucnthe176442@fpt.edu.vn>
2024-07-19 13:04:48 +0700

    -init

---
+++ Question2/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using CourseManagement.Models;
+using System.Windows;
+
+namespace CourseManagement
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var services = new ServiceCollection();
+            services.AddScoped<AppDbContext>();
+            services.AddTransient<MainWindow>();
+            var provider = services.BuildServiceProvider();
+            provider.GetRequiredService<MainWindow>().Show();
+        }
+    }
+
+}
---
+++ Question2/Models/AppDbContext.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace CourseManagement.Models;
+
+public partial class AppDbContext : DbContext
+{
+    public AppDbContext()
+    {
+    }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Course> Courses { get; set; }
+
+    public virtual DbSet<CourseCategory> CourseCategories { get; set; }
+
+    public virtual DbSet<Enrollment> Enrollments { get; set; }
+
+    public virtual DbSet<Instructor> Instructors { get; set; }
+
+    public virtual DbSet<Review> Reviews { get; set; }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (optionsBuilder.IsConfigured) return;
+        var configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", true, true).Build();
+        optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Course>(entity =>
+        {
+            entity.HasKey(e => e.CourseId).HasName("PK__Courses__8F1EF7AE08935C64");
+
+            entity.Property(e => e.CourseId).HasColumnName("course_id");
+            entity.Property(e => e.Description)
+                .HasColumnType("text")
+                .HasColumnName("description");
+            entity.Property(e => e.InstructorId).HasColumnName("instructor_id");
+            entity.Property(e => e.Title)
+                .HasMaxLength(255)
+                .IsUnicode(false)
+                .HasColumnName("title");
+
+            entity.HasOne(d => d.Instructor).WithMany(p => p.Courses)
+                .HasForeignKey(d => d.InstructorId)
+                .HasConstraintName("FK__Courses__instruc__286302EC");
+
+            entity.HasMany(d => d.Categories).WithMany(p => p.Courses)
+                .UsingEntity<Dictionary<string, object>>(
+                    "CourseCategoryAssignment",
+                    r => r.HasOne<CourseCategory>().WithMany()
+                        .HasForeignKey("CategoryId")
+                        .OnDelete(DeleteBehavior.ClientSetNull)
+                        .HasConstraintName("FK__CourseCat__categ__31EC6D26"),
+                    l => l.HasOne<Course>().WithMany()
+                        .HasForeignKey("CourseId")
+                        .OnDelete(DeleteBehavior.ClientSetNull)
+                        .HasConstraintName("FK__CourseCat__cours__30F848ED"),
+                    j =>
+                    {
+                        j.HasKey("CourseId", "CategoryId").HasName("PK__CourseCa__D24A193508282796");
+                        j.ToTable("CourseCategoryAssignments");
+                        j.IndexerProperty<int>("CourseId").HasColumnName("course_id");
+                        j.IndexerProperty<int>("CategoryId").HasColumnName("category_id");
+                    });
+        });
+
+        modelBuilder.Entity<CourseCategory>(entity =>
+        {
+            entity.HasKey(e => e.CategoryId).HasName("PK__CourseCa__D54EE9B4C764AB06");
+
+            entity.Property(e => e.CategoryId).HasColumnName("category_id");
+            entity.Property(e => e.CategoryName)
+                .HasMaxLength(255)
+                .IsUnicode(false)
+                .HasColumnName("category_name");
+        });
+
+        modelBuilder.Entity<Enrollment>(entity =>
+        {
+            entity.HasKey(e => new { e.UserId, e.CourseId }).HasName("PK__Enrollme__414FD8759DBF5D44");
+
+            entity.Property(e => e.UserId).HasColumnName("user_id");
+            entity.Property(e => e.CourseId).HasColumnName("course_id");
+            entity.Property(e => e.EnrolledDate).HasColumnName("enrolled_date");
+
+            entity.HasOne(d => d.Course).WithMany(p => p.Enrollments)
+                .HasForeignKey(d => d.CourseId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Enrollmen__cours__2C3393D0");
+
+            entity.HasOne(d => d.User).WithMany(p => p.Enrollments)
+                .HasForeignKey(d => d.UserId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Enrollmen__user___2B3F6F97");
+        });
+
+        modelBuilder.Entity<Instructor>(entity =>
+        {
+            entity.HasKey(e => e.InstructorId).HasName("PK__Instruct__A1EF56E87B54B864");
+
+            entity.Property(e => e.InstructorId).HasColumnName("instructor_id");
+            entity.Property(e => e.Bio)
+                .HasColumnType("text")
+                .HasColumnName("bio");
+            entity.Property(e => e.Name)
+                .HasMaxLength(255)
+                .IsUnicode(false)
+                .HasColumnName("name");
+        });
+
+        modelBuilder.Entity<Review>(entity =>
+        {
+            entity.HasKey(e => e.ReviewId).HasName("PK__Reviews__60883D9084BE4940");
+
+            entity.Property(e => e.ReviewId).HasColumnName("review_id");
+            entity.Property(e => e.CourseId).HasColumnName("course_id");
+            entity.Property(e => e.Rating).HasColumnName("rating");
+            entity.Property(e => e.ReviewDate).HasColumnName("review_date");
+            entity.Property(e => e.ReviewText)
+                .HasColumnType("text")
+                .HasColumnName("review_text");
+            entity.Property(e => e.UserId).HasColumnName("user_id");
+
+            entity.HasOne(d => d.Course).WithMany(p => p.Reviews)
+                .HasForeignKey(d => d.CourseId)
+                .HasConstraintName("FK__Reviews__course___35BCFE0A");
+
+            entity.HasOne(d => d.User).WithMany(p => p.Reviews)
+                .HasForeignKey(d => d.UserId)
+                .HasConstraintName("FK__Reviews__user_id__34C8D9D1");
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.UserId).HasName("PK__Users__B9BE370F090E7CA1");
+
+            entity.Property(e => e.UserId).HasColumnName("user_id");
+            entity.Property(e => e.Email)
+                .HasMaxLength(255)
+                .IsUnicode(false)
+                .HasColumnName("email");
+            entity.Property(e => e.Password)
+                .HasMaxLength(255)
+                .IsUnicode(false)
+                .HasColumnName("password");
+            entity.Property(e => e.Username)
+                .HasMaxLength(255)
+                .IsUnicode(false)
+                .HasColumnName("username");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
